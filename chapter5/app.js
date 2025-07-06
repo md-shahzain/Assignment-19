2132f0c937d498d5da07701e1ca91f14d60be6a7
md-shahzain <muhammadshahzain0341@gmail.com>
2025-07-07 01:29:13 +0500

    First ass for js

---
+++ chapter5/app.js
@@ -0,0 +1,120 @@
+// problem no 1;
+
+// let num1 = 8;
+// let num2 = 5;
+// let result = num1 + num2 ;
+// document.write("Some of" + num1 + "is" + num2 +  result);
+
+// problem no 2;
+
+// Subtraction 
+// let result = num1 - num2;
+
+
+// Multiplication 
+// let result = num1 * num2;
+
+
+// Division
+// var result = num1 / num2;
+
+
+// Modulus
+// var result = num1 % num2;
+
+
+
+
+// problem no 4;
+
+
+// let ticketPrice = 600;
+// let totalcost = ticketPrice * 5 ;
+// document.write("Total cost to buy 5 tickets to a movie is" + totalcost + "PKR");
+
+
+// problem no 5;
+
+
+// var num = 5;
+// for (var i = 1 ; i <= 10 ; i++){
+//     document.write(num + "x" + i + "=" + (num*i) + "</br>")
+// };
+
+
+// problem no 6;
+
+
+// // Celsius to Fahrenheit
+// var celsius = 25;
+// var fahrenheit = (celsius * 9/5) + 32;
+// document.write(celsius + "°C is " + fahrenheit + "°F");
+
+// // Fahrenheit to Celsius
+// var fahren = 70;
+// var cel = (fahren - 32) * 5/9;
+// document.write("<br>" + fahren + "°F is " + cel + "°C");
+
+
+// problem no 7;
+
+// var price1 = 650;
+// var price2 = 100;
+// var quantity1 = 3;
+// var quantity2 = 7;
+// var shipping = 100;
+// var total = (price1 * quantity1) + (price2 * quantity2) + shipping;
+// document.write("Total cost of your order is " + total);
+
+
+// problem no 8;
+
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = (marksObtained / totalMarks) * 100;
+// document.write("Percentage: " + percentage + "%");
+
+
+// problem no 9;
+
+
+// var usd = 10;
+// var riyal = 25;
+// var totalPKR = (usd * 104.80) + (riyal * 28);
+// document.write("Total Currency in PKR: " + totalPKR);
+
+
+// problem no 10;
+
+// var number = ((5 + 5) * 10) / 2;
+// document.write("Result: " + number);
+
+
+// problem no 11;
+
+// var currentYear = 2025;
+// var birthYear = 2005;
+// var age1 = currentYear - birthYear;
+// var age2 = age1 - 1;
+// document.write("They are either " + age2 + " or " + age1 + " years old.");
+
+
+// problem no 12;
+
+// var radius = 20;
+// var circumference = 2 * 3.142 * radius;
+// var area = 3.142 * radius * radius;
+// document.write("The circumference is " + circumference);
+// document.write("<br>The area is " + area);
+
+
+
+// problem no 13;
+
+
+// var snack = "Chips";
+// var currentAge = 15;
+// var maxAge = 65;
+// var perDay = 3;
+// var totalNeeded = (maxAge - currentAge) * 365 * perDay;
+// document.write("You will need " + totalNeeded + " " + snack + " to last you until the ripe old age of " + maxAge);
